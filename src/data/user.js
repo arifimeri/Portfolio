f64b5d3ccfbf71c8bc929102bbeae8a793e5a9a9
Arif Imeri <arifimeri.11@gmail.com>
2024-11-19 10:37:57 +0100

    Adding website

---
+++ src/data/user.js
@@ -0,0 +1,55 @@
+const INFO = {
+	main: {
+		title: "Reactfolio by Arif Imeri",
+		name: "Arif Imeri",
+		email: "Arifimeri.11@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		github: "https://github.com/arifimeri/",
+		linkedin: "https://linkedin.com/",
+		instagram: "https://www.instagram.com/arif.imeeri/",
+		facebook: "https://www.facebook.com/100000234324123O32743",
+	},
+
+	homepage: {
+		title: "Hello, I'm Arif Imeri, Frontend Developer",
+		description:
+			"I specialize in crafting modern and responsive web interfaces that deliver seamless user experiences. With a passion for clean code and elegant design, I turn ideas into functional and visually appealing digital solutions. Let's build something amazing together!",
+	},
+
+	about: {
+		title: "About Me",
+		description:
+			"I've been a programmer for three years, and throughout this journey, I've worked on diverse projects that have helped me grow my skills and expertise. As a student at Cacttus Education and a graduate of a course at Innovation Academy, I’ve gained hands-on experience and valuable insights into the tech world. Many of my projects are open for exploration, and I always welcome feedback and collaboration. Sharing ideas and working with others is an incredible way to evolve, and I'm always eager to learn and improve.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Drinori PVC",
+			description:
+				"Drinori PVC specialize in the production of high-quality doors and windows, combining durability, style, and precision. With a focus on innovation and customer satisfaction, Drinori PVC delivers solutions that enhance the comfort and aesthetics of your spaces.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://drinori-pvc.com/index.html",
+		},
+
+		{
+			title: "Doppio",
+			description:
+				"They combine exceptional service, delicious offerings, and a cozy ambiance to create unforgettable moments for you and your guests..",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "/",
+		},
+	],
+};
+
+export default INFO;
